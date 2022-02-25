4207f5a0f31665155598e4c0bac1b5846264ea2b
Nandha2397 <100400536+Nandha2397@users.noreply.github.com>
2022-02-25 11:35:15 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
