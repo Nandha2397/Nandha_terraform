29507143045dabb6c01db931b424ab4c97785df0
Nandha2397 <100400536+Nandha2397@users.noreply.github.com>
2022-02-26 19:35:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,7 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn-ami-hvm*"]
-  }
-}
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+    ami           = "ami-08e4e35cccc6189f4"
+    instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
